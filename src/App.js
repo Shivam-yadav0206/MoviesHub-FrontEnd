909c64c76ea28ef14e8edc19807e6169bda62040
Shivam Yadav <sy7176442@gmail.com>
2023-12-16 08:10:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import SearchIcon from './search.svg';
-import MovieCard from './MovieCard';
-import { Link } from 'react-router-dom';
-
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=44a329d0';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+import { Link } from "react-router-dom";
+import { API_URL } from "./config";
 const App = () => {
-    const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const searchMovies = async (title) => {
+  const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    setMovies(data.Search)
+    setMovies(data.Search);
     console.log(data);
-    }
-    useEffect(() => {
-        searchMovies('Spiderman');
-    }, []);
+  };
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
 
-    return (
-      <div className="app">
-        <h1>MoviesHub</h1>
-        <div className="search">
-          <input
-            placeholder="Search for movies"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <img
-            src={SearchIcon}
-            alt="search"
-            onClick={() => searchMovies(searchTerm)}
-          />
-        </div>
-        {movies?.length > 0 ? (
-          <div className="container">
-            {movies.map((movie) => (
-              <Link
-                to={`https://www.imdb.com/title/${movie.imdbID}/`}
-                target="_blank"
-                rel="noopener noreferrer">
-                <MovieCard movie={movie} />
-              </Link>
-            ))}
-          </div>
-        ) : (
-          <div className="empty">
-            <h2>No movies found</h2>
-          </div>
-        )}
+  return (
+    <div className="app">
+      <h1>MoviesHub</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
-    );
-}
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <Link
+              to={`https://www.imdb.com/title/${movie.imdbID}/`}
+              target="_blank"
+              rel="noopener noreferrer">
+              <MovieCard movie={movie} />
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
